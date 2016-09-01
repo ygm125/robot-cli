b96996f9c19922033a8eb03a8ff182dedabbf16a
liulou <liulou@mogujie.com>
2016-09-01 15:06:46 +0800

    up

--- template/common/config.js
+++ template/common/config.js
@@ -1,3 +1,34 @@
+let publicPath = '/dist/'
+let manifest = {}
+try {
+	manifest = require( `${ROOT_PATH}/static${publicPath}manifest.json` )
+} catch ( err ) { manifest = {} }
+
 module.exports = {
-	port: 8888
+	view: {
+		opt: {
+			globals: {
+				useJs( file ) {
+					if ( file ) {
+						if ( !/\.js$/.test( file ) ) {
+							file += '.js'
+						}
+						let filepath = `${publicPath}page/${file}`
+						if ( !DEBUG ) {
+							filepath = `${publicPath}${manifest[ file ]}`
+						}
+						return `<script src="${filepath}"></script>`
+					}
+				},
+				useCss( file ) {
+					if ( !/\.css$/.test( file ) ) {
+						file += '.css'
+					}
+					if ( !DEBUG ) {
+						return `<link href="${publicPath}${manifest[ file ]}" rel="stylesheet" type="text/css" />`
+					}
+				}
+			}
+		}
+	}
 }
--- template/common/index.js
+++ template/common/index.js
@@ -1,36 +1,40 @@
 'use strict'
 
+global.ROOT_PATH = __dirname
+
 const robot = require( 'robot-frame' )
 
-robot.setRootPath( __dirname )
+robot.preAppLoad( function () {
 
-if ( DEBUG ) {
-    // 静态资源热更新
-    require( 'babel-polyfill/node_modules/regenerator-runtime/runtime' )
-    let webpack = require( 'webpack' ),
-        webpackMiddleware = require( 'koa-webpack-middleware' ),
-        devMiddleware = webpackMiddleware.devMiddleware,
-        hotMiddleware = webpackMiddleware.hotMiddleware,
-        webpackConf = require( './webpack.config' ),
-        compiler = webpack( webpackConf )
+    if ( DEBUG ) {
+        // 静态资源热更新
+        require( 'babel-polyfill/node_modules/regenerator-runtime/runtime' )
+        let webpack = require( 'webpack' ),
+            webpackMiddleware = require( 'koa-webpack-middleware' ),
+            devMiddleware = webpackMiddleware.devMiddleware,
+            hotMiddleware = webpackMiddleware.hotMiddleware,
+            webpackConf = require( './webpack.config' ),
+            compiler = webpack( webpackConf )
 
-    robot.use( devMiddleware( compiler, {
-        noInfo: false,
-        publicPath: webpackConf.output.publicPath,
-        stats: {
-            colors: true,
-            chunks: false
-        }
-    }) )
+        robot.use( devMiddleware( compiler, {
+            noInfo: false,
+            publicPath: webpackConf.output.publicPath,
+            stats: {
+                colors: true,
+                chunks: false
+            }
+        }) )
 
-    robot.use( hotMiddleware( compiler ) )
-}
+        robot.use( hotMiddleware( compiler ) )
+    }
+})
 
 robot.run()
 
 process.on( 'uncaughtException', function ( err ) {
     Logger.error( err )
-});
+})
+
 
 
 
